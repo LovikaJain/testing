0649f3a2fa208d205d7099313b9f9eafcd493abe
Lovika Jain <lovika.jain@ipmovers.com>
2018-07-09 17:24:54 +0530

    updated new code

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-
+/* 
 var request = require('supertest');
 var app = require('../index.js');
 
@@ -7,4 +7,31 @@ describe('GET /', function() {
     // navigate to root and check the response is "hello world"
     request(app).get('/').expect('hello world', done);
   });
+});
+ */
+
+
+ var supertest = require('supertest');
+ var should = require('should');
+ 
+// This agent refers to PORT where the program is running.
+ var server = supertest.agent("http://localhost:3000");
+
+// UNIT test begin
+describe("SAMPLE unit test", function(){
+
+  // #1 should return home page
+  it("should return home page", function(done){
+    // calling home page
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(200) // This is the HTTP response
+    .end(function(err, res){
+      //  HTTP status should be 200
+      res.status.should.equal(200);
+      done();
+    });
+  });
+
 });
