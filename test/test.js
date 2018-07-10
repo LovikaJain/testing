9630bf140abdfd09babf73490ef457e8267f8926
Lovika Jain <lovika.jain@ipmovers.com>
2018-07-10 18:26:28 +0530

    updated to test webHooks

--- test/test.js
+++ test/test.js
@@ -21,7 +21,6 @@ var server = supertest.agent("http://localhost:4000");
 // UNIT test begin
 
 describe("SAMPLE unit test",function(){
-
   // #1 should return home page
   it("should return home page",function(done){
     // calling home page
@@ -35,5 +34,4 @@ describe("SAMPLE unit test",function(){
       done();
     });
   });
-
 });
